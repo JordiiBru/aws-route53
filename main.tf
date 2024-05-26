97f01585d47d08666bcaef1fb380f06b8f924785
jordi.bru <jordi.bru@mango.com>
2024-05-26 19:56:18 +0200

    fix estif

--- main.tf
+++ main.tf
@@ -4,7 +4,6 @@ data "aws_route53_zone" "jordibru_cloud" {
 
 # A record
 resource "aws_route53_record" "A_record" {
-  count   = (var.cloudfront_endpoint != "") && (var.cloudfront_zone_id != "") ? 1 : 0
   zone_id = data.aws_route53_zone.jordibru_cloud.zone_id
   name    = var.domain_name
   type    = "A"
